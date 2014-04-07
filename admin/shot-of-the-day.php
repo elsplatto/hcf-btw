d7d43de60b578ed7b74465db90de9ea9d481e8ce
Jason Taikato <jasontaikato@gmail.com>
2014-04-07 13:19:09 +1000

    Start Shot of the day admin

--- admin/shot-of-the-day.php
+++ admin/shot-of-the-day.php
@@ -5,7 +5,9 @@ include '../includes/global-functions.php';
 require '../includes/instagram.class.php';
 require '../includes/instagram.config.php';
 include 'includes/global-admin-functions.php';
-assessLogin(['super','publisher','author','restricted'])
+assessLogin(['super','publisher','author','restricted']);
+
+//function getShotOfTheDayID()
 ?>
 <html>
     <head>
@@ -15,16 +17,39 @@ assessLogin(['super','publisher','author','restricted'])
     </head>
     <body>
 
-<?php
-include 'includes/header.php';
-?>
-<section>
-    <div class="row">
-        <div class="large-12 columns">
-            <a href="dashboard.php">Dashboard</a>
-            <h1>
-                Shot of the day
-            </h1>
+    <?php
+    include 'includes/header.php';
+    ?>
+    <section>
+        <div class="row">
+            <div class="large-12 columns">
+                <a href="dashboard.php">Dashboard</a>
+                <h1>
+                    Shot of the day
+                </h1>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+
+
+    <section>
+        <div class="row">
+            <div class="large-6 columns">
+                <form action="shot-of-the-day-process.php" name="frmShotOfTheDay" id="frmShotOfTheDay" method="post">
+                    <label for="mediaId">Media ID:
+                        <input type="text" id="txtMediaId" name="txtMediaId" />
+                    </label>
+                    <input type="submit" class="button" value="Submit" />
+                </form>
+            </div>
+
+            <div class="large-6 columns">
+                here
+            </div>
+        </div>
+    </section>
+    <?php
+    include 'includes/footer.php';
+    ?>
+    </body>
+</html>
